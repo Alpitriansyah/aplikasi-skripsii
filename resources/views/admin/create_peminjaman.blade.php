b769326eb6888bb8f05c7a6953dc9378e6df4e71
Indra Wijaya <indraakmal08@gmail.com>
2023-12-21 19:01:20 +0800

    add card tambah peminjaman

--- resources/views/admin/create_peminjaman.blade.php
+++ resources/views/admin/create_peminjaman.blade.php
@@ -37,7 +37,7 @@
                                 <label for="inputState">State</label>
                                 <select id="inputState" class="form-control">
                                     <option selected>Choose...</option>
-                                    <option>Samarinda</option>
+                                    <option value="">Samarinda</option>
                                     <option>Samarinda</option>
                                     <option>Samarinda</option>
                                 </select>
