b1ef85f9ae3b8ed8a099445c3119d6d67998b473
Muhammad Alfitriansyah <sondari39@gmail.com>
2024-01-21 13:33:42 +0800

    Landing page

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -42,6 +42,7 @@
                     class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#slider">Ruangan</a></li>
                     <li class="nav-item"><a class="nav-link" href="#jadwal">Jadwal</a></li>
                     <li class="nav-item"><a class="nav-link" href="#tatib">Tata Tertib</a></li>
                     <li class="nav-item"><a class="nav-link" href="#login">Login</a></li>
@@ -60,45 +61,19 @@
 
     <section id="slider">
         <div class="container">
-            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                </ol>
+            <h2 class="text-center">Gambar Ruangan</h2>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
+                    <div class="carousel-item">
+                        <img src="{{ asset('template/img/tamu.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('template/img/tamu2.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
                     <div class="carousel-item active">
-                        <img src="asset('template/img/tamu.jpg')" class="d-block w-100" alt="gambar1">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="asset('template/img/tamu2.jpg')" class="d-block w-100" alt="gambar2">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('template/img/tamu3.jpg') }}" class="d-block w-100" alt="gambar3">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Third slide label</h5>
-                            <p>Some representative placeholder content for the third slide.</p>
-                        </div>
+                        <img src="{{ asset('template/img/tamu3.jpg') }}" class="d-block w-100" alt="...">
                     </div>
                 </div>
-                <a class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions"
-                    data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" type="button" data-target="#carouselExampleCaptions"
-                    data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
         </div>
     </section>
