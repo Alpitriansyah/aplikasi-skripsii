a8b186403c39b4cda80d9b6408637ea8eb3d55bd
Muhammad Alfitriansyah <sondari39@gmail.com>
2024-01-24 14:19:54 +0800

    change password label

--- resources/views/admin/profile/change_password.blade.php
+++ resources/views/admin/profile/change_password.blade.php
@@ -18,19 +18,38 @@
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="old_password">Old Password</label>
-                                <input type="password" class="form-control" id="inputEmail4" name="old_password">
+                                <input type="password" class="form-control" id="old_password" name="old_password">
+                                @error('old_password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="new_password">New Password</label>
-                                <input type="password" class="form-control" id="inputEmail4" name="new_password">
+                                <input type="password"
+                                    class="form-control @error('new_password')
+                                    is-invalid @enderror"
+                                    id="new_password" name="new_password">
+                                @error('new_password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="repeat_password">Repeat Password</label>
-                                <input type="password" class="form-control" id="inputEmail4" name="repeat_password">
+                                <input type="password" class="form-control @error('repeat_password') is-invalid @enderror"
+                                    id="repeat_password" name="repeat_password">
+                                @error('repeat_password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary">Ubah</button>
